a798540aa783758d75a5d2d1120e53e892250b25
querlemtle <citta120120@gmail.com>
2022-04-01 20:40:49 +0800

    build static page

---
+++ app.js
@@ -0,0 +1,28 @@
+// 從node_modules加入express、express-handlebars
+const express = require('express');
+const app = express();
+const exphbs = require('express-handlebars');
+const restaurantList = require('./restaurant.json');
+
+// 伺服器埠號
+const port = 3000;
+
+// 設定樣板引擎
+app.set('view engine', 'handlebars');
+app.engine('handlebars', exphbs.engine({ defaultLayout: 'main' }));
+
+// 設定靜態檔案
+app.use(express.static('public'));
+
+// 設定路由
+app.get('/', (req, res) => {
+  res.render('index');
+});
+
+app.get('/restaurants/:restaurant_id', (req, res) => {
+  res.render('show');
+});
+
+app.listen(port, () => {
+  console.log(`Express is listening on http://localhost:${port}`);
+});
