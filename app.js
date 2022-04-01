5b9b7842306848bd33aa6076752c74f6d578ef76
querlemtle <citta120120@gmail.com>
2022-04-01 22:39:53 +0800

    add search function

--- app.js
+++ app.js
@@ -19,6 +19,15 @@ app.get('/', (req, res) => {
   res.render('index', { restaurants: restaurantList.results });
 });
 
+app.get('/search', (req, res) => {
+  const keyword = req.query.keyword;
+  const restaurants = restaurantList.results.filter(restaurant => {
+    return restaurant.name.toLowerCase().includes(keyword) || 
+    restaurant.category.toLowerCase().includes(keyword);
+  });
+  res.render('index', { restaurants: restaurants, keyword: keyword });
+});
+
 app.get('/restaurants/:restaurant_id', (req, res) => {
   const restaurant = restaurantList.results.find(restaurant => restaurant.id.toString() === req.params.restaurant_id);
   res.render('show', { restaurant: restaurant });
